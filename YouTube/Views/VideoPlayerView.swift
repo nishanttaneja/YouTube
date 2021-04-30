229281484b8dca5e78e0f8b7b4640bdda5638448
Nishant Taneja <nishant.taneja2701@gmail.com>
2021-04-30 23:08:55 +0530

    Add ControlsContainer For VideoPlayer

--- YouTube/Views/VideoPlayerView.swift
+++ YouTube/Views/VideoPlayerView.swift
@@ -9,23 +9,93 @@ import UIKit
 import AVFoundation
 
 class VideoPlayerView: UIView {
+    private var player: AVPlayer?
+    private var isPlaying: Bool = false
+    
+    private lazy var controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .init(white: 0, alpha: 0.5)
+        return view
+    }()
+    
+    private lazy var activityIndicatorView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(style: .large)
+        view.color = .white
+        view.startAnimating()
+        return view
+    }()
+    
+    private lazy var playPauseButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "pause"), for: .normal)
+        button.tintColor = .white
+        button.isHidden = true
+        button.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
-        let urlString = "https://reflexioncdn.azureedge.net/contentdata/MEDIA/296/Proxy_Video/296_854x480.mp4"
-        if let url = URL(string: urlString) {
-            let player = AVPlayer(url: url)
-            
-            let playerLayer = AVPlayerLayer(player: player)
-            self.layer.addSublayer(playerLayer)
-            playerLayer.frame = self.frame
-            
-            player.play()
-        }
+        configurePlayerView()
+        configureControlsContainerView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension VideoPlayerView {
+    private func configurePlayerView() {
+        let urlString = "https://reflexioncdn.azureedge.net/contentdata/MEDIA/296/Proxy_Video/296_854x480.mp4"
+        if let url = URL(string: urlString) {
+            player = AVPlayer(url: url)
+            let playerLayer = AVPlayerLayer(player: player)
+            self.layer.addSublayer(playerLayer)
+            playerLayer.frame = self.frame
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+            player?.play()
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            isPlaying = true
+            playPauseButton.isHidden = false
+        }
+    }
+    
+    private func configureControlsContainerView() {
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        // Activity Indicator View
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        // PlayPause Button
+        controlsContainerView.addSubview(playPauseButton)
+        playPauseButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            playPauseButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            playPauseButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            playPauseButton.widthAnchor.constraint(equalToConstant: 50),
+            playPauseButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc func handlePlayPause() {
+        if isPlaying {
+            player?.pause()
+            playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+        } else {
+            player?.play()
+            playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+        }
+        isPlaying = !isPlaying
     }
 }
